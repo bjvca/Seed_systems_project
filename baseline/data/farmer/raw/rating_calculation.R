6c0baee0bbef14a82de0bcd4ee4230caa3359123
Caroline Miehe <caroline.miehe@kuleuven.be>
2021-05-26 15:56:19 +0200

    added gender analysis below

--- baseline/data/farmer/raw/rating_calculation.R
+++ baseline/data/farmer/raw/rating_calculation.R
@@ -68,4 +68,96 @@ reviews$score <-  rowMeans(reviews[c("quality_rating","general","yield","drought
 path <- strsplit(path, "/farmer")[[1]]
 write.csv(reviews, paste(path, "agro_input/raw/shiny_app_seed/reviews_seed.csv",sep="/"), row.names=FALSE)
 
+#######################################################
+#differences between ratings of male & female dealers?#
+#######################################################
 
+#merge with dealer baseline data to get gender variable
+baseline_dealer <- read.csv("C:/Users/u0127963/Desktop/PhD/Seed_systems_project/baseline/data/agro_input/public/baseline_dealer.csv")
+baseline_dealer_with_score <- merge(reviews, baseline_dealer, by="shop_ID")
+
+#create gender dummy
+baseline_dealer_with_score$genderdummy <- ifelse(baseline_dealer_with_score$maize.owner.agree.gender == "Male", 1, 0)
+summary(baseline_dealer_with_score$genderdummy)
+
+#control for age, education, distance of to nearest tarmac road, distance to nearest murram road
+summary(baseline_dealer_with_score$maize.owner.agree.age)
+baseline_dealer_with_score$prim <- FALSE
+baseline_dealer_with_score$prim <- (baseline_dealer_with_score$maize.owner.agree.educ %in% c("c","d","e","f"))
+summary(baseline_dealer_with_score$prim)
+baseline_dealer_with_score$maize.owner.agree.q3[baseline_dealer_with_score$maize.owner.agree.q3==999] <- NA
+summary(baseline_dealer_with_score$maize.owner.agree.q3)
+summary(baseline_dealer_with_score$maize.owner.agree.q4)
+
+summary(lm(score~genderdummy + maize.owner.agree.age + prim + maize.owner.agree.q3 + maize.owner.agree.q4, data = baseline_dealer_with_score))
+
+
+
+#additionally control for
+#Q5. Is this a specialized agro-input shop that only sells farm inputs?
+summary(baseline_dealer_with_score$maize.owner.agree.q5)
+
+#Q8. When was this agro-input shop established? (year)
+baseline_dealer_with_score$years_shop <- 2020 - as.numeric(as.character(substr(baseline_dealer_with_score$maize.owner.agree.q8, start=1, stop=4)))
+summary(baseline_dealer_with_score$years_shop)
+
+#Q69. Are seed stored in a dedicated area, away from other merchandize?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q69)
+
+#Q71. Do you have a problem with rats or pests (insects, rats)?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q71)
+
+#Q72. Is the roof leak-proof?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q72)
+
+#Q73. Is the roof insulated to keep heat out?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q73)
+
+#Q74. Are the walls insulated to keep the heat out?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q74)
+
+#Q75. Is the area ventilated
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q75)
+
+#Q76. Are the walls plastered?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q76)
+
+#Q77. Material of floor in areas where seed is stored?
+baseline_dealer_with_score$goodfloor <- FALSE
+baseline_dealer_with_score$goodfloor <- (baseline_dealer_with_score$maize.owner.agree.temp.q77 %in% c("Cement","Tiles"))
+summary(baseline_dealer_with_score$goodfloor)
+
+#Q78. Lighting conditions in area where seed is stored?
+baseline_dealer_with_score$badlighting <- FALSE
+baseline_dealer_with_score$badlighting <- (baseline_dealer_with_score$maize.owner.agree.temp.q78 %in% c("1"))
+summary(baseline_dealer_with_score$badlighting)
+
+#Q79. On what surface are seed stored?
+baseline_dealer_with_score$badstored <- FALSE
+baseline_dealer_with_score$badstored <- (baseline_dealer_with_score$maize.owner.agree.temp.q79 %in% c("1", "2", "96"))
+summary(baseline_dealer_with_score$badstored)
+
+#Q80. Do you see maize seed that is stored in open bags or open containers?
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q80)
+
+#Q81. Do you see any official certificates displayed in the store (eg that the shop was inspected ,that the owner attended trainings or that the business is registered with some association)
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q81)
+
+#Q82. On a scale of 1 to 5, rate this shop in terms of cleanness and professionality 1 poor 5 excellent
+as.numeric(as.character(baseline_dealer_with_score$maize.owner.agree.temp.q82))
+summary(baseline_dealer_with_score$maize.owner.agree.temp.q82)
+
+# #Q92. When repackaging seed, do you keep track of expiry date (eg include it in the bag/write it on the bag)
+# baseline_dealer_with_score$maize.owner.agree.q92[baseline_dealer_with_score$maize.owner.agree.q92=="n/a"] <- NA
+# summary(baseline_dealer_with_score$maize.owner.agree.q92)
+
+#Q96. Since last season, did you receive any complaint from a customer that seed you sold was not good?
+summary(baseline_dealer_with_score$maize.owner.agree.q96)
+
+#Q70. Entert the temperature in the seed store (where seed is stored)
+summary(baseline_dealer_with_score$maize.owner.agree.q70)
+
+# #moisture
+# summary(baseline_dealer_with_score$reading)
+
+summary(lm(score~genderdummy + maize.owner.agree.age + prim + maize.owner.agree.q3 + maize.owner.agree.q4 + maize.owner.agree.q5 + years_shop + maize.owner.agree.temp.q69 + maize.owner.agree.temp.q71 + maize.owner.agree.temp.q72 + maize.owner.agree.temp.q73 + maize.owner.agree.temp.q74 + maize.owner.agree.temp.q75 + maize.owner.agree.temp.q76 + goodfloor + badlighting + badstored + maize.owner.agree.temp.q80 + maize.owner.agree.temp.q81 + maize.owner.agree.temp.q82 + maize.owner.agree.q96 + maize.owner.agree.q70, data = baseline_dealer_with_score))
