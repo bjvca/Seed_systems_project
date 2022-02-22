c73562d708527a39e7e65dfd63ce1452bb5a0db0
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2022-02-22 17:28:31 +0300

    removed duplicates in input shop IDs

--- midline/data/agro_input/raw/anonymize.R
+++ midline/data/agro_input/raw/anonymize.R
@@ -7,6 +7,31 @@ path <- getwd()
 
 ### reads in raw data (not public)
 shops <- read.csv(paste(path,"Dealer_MidlineV4_2022_02_16_03_40_40_437618-1.csv", sep="/"))
+shops$shop_ID <- as.character(shops$shop_ID)
+### five duplicate... four shops have ID == "n/a" and these n/a's also do not have "clearing" and "trainin" status... add here?
+shops$shop_ID[shops$X_uuid =="a3faaf4d-2704-4b80-953a-b968579fe6b9"] <- "AD_131"
+shops$clearing[shops$X_uuid =="a3faaf4d-2704-4b80-953a-b968579fe6b9"] <- TRUE
+shops$training[shops$X_uuid =="a3faaf4d-2704-4b80-953a-b968579fe6b9"] <- FALSE
+
+shops$shop_ID[shops$X_uuid =="3f435fb7-4832-4fb3-83bd-be1ae701db6e"] <- "AD_306"
+shops$clearing[shops$X_uuid =="3f435fb7-4832-4fb3-83bd-be1ae701db6e"] <- FALSE
+shops$training[shops$X_uuid =="3f435fb7-4832-4fb3-83bd-be1ae701db6e"] <- TRUE
+
+shops$shop_ID[shops$X_uuid =="2c0f8acf-b804-4816-9545-ff36b332cddb"] <- "AD_65"
+shops$clearing[shops$X_uuid =="2c0f8acf-b804-4816-9545-ff36b332cddb"] <- TRUE
+shops$training[shops$X_uuid =="2c0f8acf-b804-4816-9545-ff36b332cddb"] <- FALSE
+
+shops$shop_ID[shops$X_uuid =="dc8dc036-8ae8-41e7-a669-48af3e02da74"] <- "AD_270"
+shops$clearing[shops$X_uuid =="dc8dc036-8ae8-41e7-a669-48af3e02da74"] <- TRUE
+shops$training[shops$X_uuid =="dc8dc036-8ae8-41e7-a669-48af3e02da74"] <- FALSE
+
+shops$shop_ID[shops$X_uuid =="f1efbe32-f4e9-44b1-b2dd-97a38c33897e"] <- "AD_279"
+shops$clearing[shops$X_uuid =="f1efbe32-f4e9-44b1-b2dd-97a38c33897e"] <- TRUE
+shops$training[shops$X_uuid =="f1efbe32-f4e9-44b1-b2dd-97a38c33897e"] <- FALSE
+
+### this one seems to be a duplicate - wife was interviewed by a different enumerator later in the day...
+shops <- subset(shops, X_uuid != "1686391d-9793-46bb-beb4-26922ac42dd7")
+
 
 ### code to determine remnants
 #need to get raw baseline data
@@ -59,12 +84,18 @@ moisture$barcode[moisture$barcode =="1216189"] <- 252
 moisture$barcode[moisture$barcode =="44446680"] <- 134
 moisture$barcode[moisture$barcode =="747116"] <- 258
 moisture$barcode[moisture$id =="AD 25942"] <- 187
-shops$owner.agree.barcode[shops$owner.agree.barcode=="16344495"] <- 298 
+moisture$barcode[moisture$id =="AD 11043"] <- 177
+moisture$barcode[moisture$id =="AD 3448"] <- 320
+shops$owner.agree.barcode[shops$owner.agree.barcode=="16344495"] <- 298
+shops$owner.agree.barcode[shops$owner.agree.barcode=="11217770"] <- 318 
 
 
 shops_check <- merge(shops, moisture,by.x="owner.agree.barcode",by.y="barcode")
 
 moisture[!(moisture$barcode %in% shops_check$owner.agree.barcode),]
+shops[!(shops$owner.agree.barcode %in% shops_check$owner.agree.barcode),]
+
+
 
 shops <- merge(shops, moisture,by.x="owner.agree.barcode",by.y="barcode", all.x=T)
 
