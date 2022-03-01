ed52e5b5b1c8575f08b1e31d42182fec51ce2760
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2022-03-01 11:27:21 +0300

    data cleaning: merged moisture measurements to agro-input dealer midline data

--- midline/data/agro_input/raw/anonymize.R
+++ midline/data/agro_input/raw/anonymize.R
@@ -31,6 +31,8 @@ shops$training[shops$X_uuid =="f1efbe32-f4e9-44b1-b2dd-97a38c33897e"] <- FALSE
 
 ### this one seems to be a duplicate - wife was interviewed by a different enumerator later in the day...
 shops <- subset(shops, X_uuid != "1686391d-9793-46bb-beb4-26922ac42dd7")
+#this is a really strange one: duplicate of 
+shops <- subset(shops, shop_ID !="AD_323")
 
 
 ### just to try
@@ -52,11 +54,11 @@ summary(lm(tot_kg~clearing, data=shops))
 
 #merge in moisture
 
-moisture <- read.csv(paste(path,"Moisture_Midline_2022_02_20_05_23_34_994254.csv", sep="/"))
+moisture <- read.csv(paste(path,"Moisture_Midline_2022_02_28_00_33_25_696986.csv", sep="/"))
 
 
-moisture$age <- difftime(strptime("01.02.2022", format = "%d.%m.%Y"),strptime(moisture$date_pack,format="%Y-%m-%d"),units="days")
-moisture$age[moisture$date_pack=="n/a"] <- difftime(strptime("01.02.2022", format = "%d.%m.%Y"),strptime(moisture$exp[moisture$date_pack=="n/a"] ,format="%Y-%m-%d"),units="days")+180
+moisture$age <- difftime(strptime("01.03.2022", format = "%d.%m.%Y"),strptime(moisture$date_pack,format="%Y-%m-%d"),units="days")
+moisture$age[moisture$date_pack=="n/a"] <- difftime(strptime("01.03.2022", format = "%d.%m.%Y"),strptime(moisture$exp[moisture$date_pack=="n/a"] ,format="%Y-%m-%d"),units="days")+180
 
 moisture$age <- as.numeric(as.character(moisture$age))
 shops$owner.agree.barcode <- as.numeric(as.character(shops$owner.agree.barcode))
@@ -65,27 +67,29 @@ moisture$barcode <- as.numeric(as.character(moisture$barcode))
 ##manually fix here to make sure all moisture measurements are used!
 moisture <- moisture[c("barcode","id","reading","exp","date_pack","origin","cert","lot","verif","variety","other_var","company","age")]
 
-moisture$barcode[is.na(moisture$barcode)] <- 251
+
+moisture$barcode[is.na(moisture$barcode) & moisture$id=="AD 24332"] <- 21
+moisture$barcode[is.na(moisture$barcode) & moisture$id=="AD  17232"] <- 30
+moisture$barcode[is.na(moisture$barcode) & moisture$id=="AD 8426"] <- 251
 
 moisture$barcode[moisture$barcode =="1216189"] <- 252
 moisture$barcode[moisture$barcode =="44446680"] <- 134
-moisture$barcode[moisture$barcode =="747116"] <- 258
-moisture$barcode[moisture$id =="AD 25942"] <- 187
 moisture$barcode[moisture$id =="AD 11043"] <- 177
 moisture$barcode[moisture$id =="AD 3448"] <- 320
+moisture$barcode[moisture$barcode =="747116"] <- 258
+moisture$barcode[moisture$id =="AD 25942"] <- 187
+
+
 shops$owner.agree.barcode[shops$owner.agree.barcode=="16344495"] <- 298
 shops$owner.agree.barcode[shops$owner.agree.barcode=="11217770"] <- 318 
 
-
-shops_check <- merge(shops, moisture,by.x="owner.agree.barcode",by.y="barcode")
-
-moisture[!(moisture$barcode %in% shops_check$owner.agree.barcode),]
-shops[!(shops$owner.agree.barcode %in% shops_check$owner.agree.barcode),]
-
+shops$owner.agree.barcode[shops$X_uuid=="bff4c5fe-77b2-405e-8142-4d0b15b87552"] <- NA
 
 
 shops <- merge(shops, moisture,by.x="owner.agree.barcode",by.y="barcode", all.x=T)
 
+#just to try
+summary(lm(reading~clearing, data=shops))
 
 to_drop <- c( "enumerator", "district", "sub","parish","village", "hh_namex","hh_name",   "shed", "phone1","q1" , "q1a","q1b","owner.consent", "owner.agree.respondent",  "q2",
 "owner.consent","owner.agree.respondent","owner.agree.nickname" ,"owner.agree.phone1", "owner.agree.phone2",      "biz_name" ,"clearing" ,"training","start","end", "start.geopoint", "X_start.geopoint_latitude","X_start.geopoint_longitude","X_start.geopoint_altitude","X_start.geopoint_precision","owner.agree.gps",   "owner.agree._gps_latitude",   "owner.agree._gps_longitude" ,"owner.agree._gps_altitude",   "owner.agree._gps_precision", "owner.agree.id",   "meta.instanceID", "X_id",     "X_uuid" , "X_submission_time", "X_date_modified", "X_tags",    "X_notes" ,  "X_version",  "X_duration" , "X_submitted_by","X_total_media",  "X_media_count", "X_media_all_received" ,     "X_xform_id",   "id", "owner.agree.barcode")
